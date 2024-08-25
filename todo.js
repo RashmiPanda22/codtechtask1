8de92977a5550929e546777bb49d2367cd29160a
RashmiPanda22 <Rashmipanda591@gmail.com>
2024-08-25 18:33:00 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,17 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add(){
+    if(inputs.value == ""){
+        alert("please Enter Task")   
+    }else{
+        let newEle = document.createElement("ul");
+        newEle.innerHTML=`${inputs.value} <i class="ri-delete-bin-6-fill"></i>`;
+        text.appendChild(newEle);
+        inputs.value="";
+        newEle.querySelector("i").addEventListener("click" , remove);
+        function remove(){
+            newEle.remove()
+        }
+    }
+}
